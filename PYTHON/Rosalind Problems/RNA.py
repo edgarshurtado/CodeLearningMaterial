5dddb8e446dfc0f2a5611b4caf519a3db9e89f7b
Edgar S. Hurtado <Rumil@MacBook-Pro-de-Edgar-2.local>
2014-12-18 09:33:58 +0100

    RNA problem

--- PYTHON/Rosalind Problems/RNA.py
+++ PYTHON/Rosalind Problems/RNA.py
@@ -1,1 +1,12 @@
-fhand=open("dataset.txt")
+fhand=open("rosalind_rna.txt")
+
+result=""
+
+for line in fhand:
+	for nBase in line:
+		if nBase == 'T':
+			nBase = 'U'
+		result+=nBase
+
+print result
+
