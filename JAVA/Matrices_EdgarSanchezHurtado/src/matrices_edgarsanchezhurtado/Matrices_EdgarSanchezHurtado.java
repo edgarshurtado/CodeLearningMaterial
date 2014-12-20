7e69dd01c8eac1045da12e478fccec284b23080a
Edgar S. Hurtado <Rumil@MacBook-Pro-de-Edgar-2.local>
2014-12-20 17:55:54 +0100

    Created new project
    Project to do several matrix operations. Like addition, subtraction and
    product.

---
+++ JAVA/Matrices_EdgarSanchezHurtado/src/matrices_edgarsanchezhurtado/Matrices_EdgarSanchezHurtado.java
@@ -0,0 +1,33 @@
+/*
+ * Program to make matrix operations
+*/
+
+package matrices_edgarsanchezhurtado;
+
+import java.io.*;
+
+/**
+ *
+ * @author Edgar S. Hurtado
+ */
+
+
+public class Matrices_EdgarSanchezHurtado {
+
+    
+    static void menu() {
+        System.out.println("1. New Matrix");
+        System.out.println("2. View matrix");
+        System.out.println("3. Matrix addition");
+        System.out.println("4. Matrix substraction");
+        System.out.println("5. Matrix product by a number");
+        System.out.println("6. Matrix product");
+        System.out.println("7. Transposed matrix");
+        
+}
+    
+    public static void main(String[] args) {
+       
+    }
+
+}
