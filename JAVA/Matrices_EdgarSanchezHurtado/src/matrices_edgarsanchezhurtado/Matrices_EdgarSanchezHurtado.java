4c7b562cc038bbfa072a0ca13641a3cef7e5e5bd
Edgar S. Hurtado <Rumil@MacBook-Pro-de-Edgar-2.local>
2014-12-24 11:22:53 +0100

    Set global variables

--- JAVA/Matrices_EdgarSanchezHurtado/src/matrices_edgarsanchezhurtado/Matrices_EdgarSanchezHurtado.java
+++ JAVA/Matrices_EdgarSanchezHurtado/src/matrices_edgarsanchezhurtado/Matrices_EdgarSanchezHurtado.java
@@ -13,9 +13,32 @@ import java.io.*;
 
 
 public class Matrices_EdgarSanchezHurtado {
-
+    //GLOBAL VARIABLES
+    static final int maxMatrices = 100;
+    static final int maxMatrixSize = 10;
+    static final int totalMatrices = 0;
+    static int matricesVector[][][] = 
+            new int[maxMatrices][maxMatrixSize][maxMatrixSize];
+    static int matricesSizes[][][] = new int[maxMatrices][1][1];
     
+    //USEFUL FUNCTIONS
+    static String rawInput(){
+        BufferedReader dataIn = new BufferedReader
+                (new InputStreamReader(System.in));
+        String temp ="";
+        
+        try {
+            temp = dataIn.readLine();
+        } catch (Exception e) {
+        }
+        
+        return temp;
+    } 
+
+    //PROGRAM LOGIC
     static void menu() {
+        System.out.println("-------------MENU------------");
+        System.out.println("");
         System.out.println("1. New Matrix");
         System.out.println("2. View matrix");
         System.out.println("3. Matrix addition");
@@ -23,11 +46,29 @@ public class Matrices_EdgarSanchezHurtado {
         System.out.println("5. Matrix product by a number");
         System.out.println("6. Matrix product");
         System.out.println("7. Transposed matrix");
+        System.out.println("8. Exit");
         
-}
+    }
+    static void newMatrix(){
+        int cols;
+        int rows;
+        int number;
+        
+        System.out.print("Nº of rows for the new matrix: ");
+        rows = Integer.parseInt(rawInput());
+        
+        System.out.print("Nº of columns for the new matrix: ");
+        cols = Integer.parseInt(rawInput());
+        
+    }
     
+    //MAIN
     public static void main(String[] args) {
        
+        
+       
+        
+       newMatrix();
     }
 
 }
