65c5ad0d2e9c911a56e44fc631c86fdf83ec8ef7
Edgar S. Hurtado <edsanhu@gmail.com>
2015-01-12 12:20:54 +0100

    named main functions

--- JAVA/NumerosEnLletres/edgarSanchezHurtadoNumerosEnLletra/src/edgarsanchezhurtadonumerosenlletra/EdgarSanchezHurtadoNumerosEnLletra.java
+++ JAVA/NumerosEnLletres/edgarSanchezHurtadoNumerosEnLletra/src/edgarsanchezhurtadonumerosenlletra/EdgarSanchezHurtadoNumerosEnLletra.java
@@ -14,6 +14,25 @@ import java.io.*;
 
 public class EdgarSanchezHurtadoNumerosEnLletra {
 
+    static int digitUnitats(int q){
+        
+    }
+    
+    static int digitDesenes(int q){
+        
+    }
+    
+    static int digitCentenes(int q){
+        
+    }
+    
+    static String desenaEnLletres(int dig){
+        
+    }
+    
+    static String digitEnLletres(int dig){
+        
+    }
     
     public static void main(String[] args) {
        
