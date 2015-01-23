97b27f6160267618929deb9c2be310c485c060ce
Edgar S. Hurtado <edsanhu@gmail.com>
2015-01-23 12:18:41 +0100

    Created Minesweeper project

---
+++ JAVA/Minesweeper/src/minesweeper/EdgarSanchezHurtado_buscaminas.java
@@ -0,0 +1,22 @@
+/*
+    Exercise description
+*/
+
+package minesweeper;
+
+import java.io.*;
+
+/**
+ *
+ * @author Edgar S. Hurtado
+ */
+
+
+public class EdgarSanchezHurtado_buscaminas {
+
+    
+    public static void main(String[] args) {
+       
+    }
+
+}
