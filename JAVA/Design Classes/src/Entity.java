613cbcffbb79c08c91fe196d6190aee973a2350d
Edgar S. Hurtado <edsanhu@gmail.com>
2015-02-15 17:22:43 +0100

    Create new project for Entornos de Desarrollo

---
+++ JAVA/Design Classes/src/Entity.java
@@ -0,0 +1,36 @@
+
+public class Entity {
+	private float[] position;
+	private boolean isStatic;
+	private boolean isVisible;
+	private long ID;
+	private boolean isKinematic;
+	
+	public Entity(){
+		
+	}
+	
+	public void print(){
+		
+	}
+	
+	public void changePosition(){
+		
+	}
+	
+	public boolean isStatic(){
+		return this.isStatic;
+	}
+	
+	public boolean isVisible(){
+		return this.isVisible;
+	}
+	
+	public boolean isKinematic() {
+		return this.isKinematic;
+	}
+	
+	public long getID() {
+		return this.ID;
+	}
+}
