de081bf4ba9d651c45d24a68f351091a0ac92fb1
Edgar S. Hurtado <edsanhu@gmail.com>
2015-02-18 12:55:34 +0100

    create Library and first class for keyboard inputs

---
+++ JAVA/UsefulFunctions/src/es/edgarsh/usefulfunctions/KeyboardInput.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.edgarsh.usefulfunctions;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Rumil
+ */
+public class KeyboardInput {
+    
+    static BufferedReader dataIn = new BufferedReader
+        (new InputStreamReader(System.in));
+    
+    static int intImput(){
+        //Function for allow keyboard integer inputs
+        
+        int tempInt = 0;
+        boolean correctInput = false;
+        while(!correctInput){
+            try {
+                tempInt = Integer.parseInt(dataIn.readLine());
+                correctInput = true;
+            } catch (IOException | NumberFormatException e) {
+                System.out.println("Bad input, try again");
+            }
+        }
+        return tempInt;
+    } 
+    
+    static String strInput(){
+        String tempInput = "";
+        boolean correctInput = false;
+        while(!correctInput){
+            try {
+                tempInput = dataIn.readLine();
+                correctInput = true;
+            } catch (IOException | NumberFormatException e) {
+                System.out.println("Bad input, try again");
+            }
+        }
+        return tempInput;
+    }
+}
