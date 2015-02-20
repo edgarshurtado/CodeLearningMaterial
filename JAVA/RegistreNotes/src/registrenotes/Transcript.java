bba33fdba6c868beb76a0346647cab5e9988b950
Edgar S. Hurtado <edsanhu@gmail.com>
2015-02-20 12:00:24 +0100

    Implement setSingleMark method

--- JAVA/RegistreNotes/src/registrenotes/Transcript.java
+++ JAVA/RegistreNotes/src/registrenotes/Transcript.java
@@ -17,8 +17,29 @@ public class Transcript {
      * Changes a single mark in the transcript
      * @param eval A value between 1 and 3.
      * @param subject The code of a subject. Choose between PRG, BDA or EDD
+     * @param newMark The mark to add.
+     * @throws Error if the newMark es negative or either eval or subject are 
+     * incorrect.
      */
-    public void setSingleMark(int eval, String subject) {
+    public void setSingleMark(int newMark, int eval, String subject) {
+        int subjectIndex = -1;
+        int evalIndex = eval -1;
         
+        //Error control
+        if (newMark < -1) throw new Error();
+        
+        if (evalIndex < 0 || evalIndex >= Transcript.subjects.length) {
+            throw new Error();
+        }
+        
+        for (int i = 0; i < Transcript.subjects.length; i++) {
+            if (subject.equals(subjects[i])) {
+                subjectIndex = i;                        
+            }
+        }
+        if (subjectIndex == -1) throw new Error();
+        
+        // Value assignment
+        this.transcript[subjectIndex][evalIndex] = newMark;
     }
 }
