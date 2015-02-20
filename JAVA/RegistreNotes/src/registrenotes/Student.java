a6bff71ae6edb9ceb087cdbf5cd515f396b11c78
Edgar S. Hurtado <edsanhu@gmail.com>
2015-02-20 17:38:42 +0100

    add Student getters and setters

--- JAVA/RegistreNotes/src/registrenotes/Student.java
+++ JAVA/RegistreNotes/src/registrenotes/Student.java
@@ -17,10 +17,50 @@ public class Student {
     private String adress;
     private int number;
     private int cp;
+
+    public int getTelef() {
+        return telef;
+    }
+
+    public void setTelef(int telef) {
+        this.telef = telef;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+        this.cp = cp;
+    }
     private Transcript transcript;
     
     public Student(){
-        
+        this.transcript = new Transcript();
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
     }
    
     
