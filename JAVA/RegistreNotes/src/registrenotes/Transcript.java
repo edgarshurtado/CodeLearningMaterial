7dce13ad21f2cb94e7cbacd726bc98a333b5ee86
Edgar S. Hurtado <edsanhu@gmail.com>
2015-02-20 11:31:04 +0100

    define method setSingleMark

--- JAVA/RegistreNotes/src/registrenotes/Transcript.java
+++ JAVA/RegistreNotes/src/registrenotes/Transcript.java
@@ -10,5 +10,15 @@ package registrenotes;
  * @author Rumil
  */
 public class Transcript {
+    private static String[] subjects = {"PRG", "BDA", "EDD"};
+    private int[][] transcript = new int[3][3];
     
+    /**
+     * Changes a single mark in the transcript
+     * @param eval A value between 1 and 3.
+     * @param subject The code of a subject. Choose between PRG, BDA or EDD
+     */
+    public void setSingleMark(int eval, String subject) {
+        
+    }
 }
