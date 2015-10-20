7d4f6bb41b70ce287ddcf1292cbcfc0b1438488d
Edgar S. Hurtado <edsanhu@gmail.com>
2015-10-20 22:56:51 +0200

    add bonfire 1

---
+++ MOOCs/FreeCodeCamp/Bonfires/MeetBonfire.js
@@ -0,0 +1,8 @@
+function meetBonfire(argument) {
+	//Good luck!
+	console.log("you can read this function's argument in the developer tools", argument);
+
+	return true;
+}
+
+meetBonfire("You can do this!");
