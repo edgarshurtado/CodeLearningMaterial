553b154e7648d87ca5fdc2f1b86f61508075cd21
Edgar S. Hurtado <edsanhu@gmail.com>
2015-10-23 20:00:38 +0200

    do proyecto DOM

---
+++ 2DAW/Desarrollo_Entorno_Cliente/proyecto DOM/main.js
@@ -0,0 +1,42 @@
+function reveal(id){
+  document.getElementById(id).className = 'visible';
+}
+
+function hide(id){
+  document.getElementById(id).className = 'hidden';
+}
+
+function changeImage(champ){
+  var champLinks = {
+    'sona' : "./image/Sona_0.jpg",
+    'thresh': "./image/Thresh_0.jpg"
+  }
+
+  document.getElementById('imageHolder').src = champLinks[champ];
+}
+
+
+function alertMessage(id){
+  var parent = document.getElementById(id);
+  var newChild = document.createElement('p');
+  newChild.className = 'important';
+  var text = document.createTextNode('Remember that AP supports are squissy');
+
+  newChild.appendChild(text);
+  parent.insertBefore(newChild, parent.getElementsByTagName('p')[1]);
+}
+
+function destroyMessage(id){
+  var parent = document.getElementById(id);
+
+  parent.removeChild(parent.getElementsByTagName('p')[1]);
+}
+
+function counters(countersArray){
+  var message = "This champ counters are :\n"
+
+  for (var i = 0; i < countersArray.length; i++) {
+    message +="-- " + countersArray[i] + "\n";
+  };
+  alert(message);
+}
