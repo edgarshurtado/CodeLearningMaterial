6bcfec8517538ddc32bbb9a35462484417f51eb4
Edgar S. Hurtado <edsanhu@gmail.com>
2015-10-26 22:10:28 +0100

    add class 26/10/2015

--- 2DAW/BackEnd/clases/ClaseCodeIgnite/CodeIgniter-3.0.2/application/config/routes.php
+++ 2DAW/BackEnd/clases/ClaseCodeIgnite/CodeIgniter-3.0.2/application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
-$route['(:any)'] = 'welcome/$1'; //lo que sea lo mapeará al nombre del método
+$route['default_controller'] = 'stores';
+$route['(:any)'] = 'stores/$1'; //lo que sea lo mapeará al nombre del método
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- 2DAW/BackEnd/clases/ClaseCodeIgnite/CodeIgniter-3.0.2/application/controllers/stores.php
+++ 2DAW/BackEnd/clases/ClaseCodeIgnite/CodeIgniter-3.0.2/application/controllers/stores.php
@@ -8,4 +8,25 @@ class Stores extends CI_Controller {
 		//Cargar el modelo
 		$this->load->model('stores_model');
 	}
+
+  public function index()
+  {
+    echo "Hola, función de entrada del controlador";
+    $titulo = "Este es el título de la primera página";
+    $cuerpo = "Este es el cuerpo de la primera página";
+
+    //Forma correcta de construir el array de datos
+    $datos = array(
+      'titulo'=> $titulo,
+      'cuerpo'=>$cuerpo
+    );
+    //Forma menos profesional
+    // $datos['titulo'] = $titulo;
+    // $datos['cuerpo'] = $cuerpo;
+
+    //Llamada del modelo
+    $datos['stores'] = $this->stores_model->getAllStores();
+
+    $this->load->view('vista', $datos);
+  }
 }
