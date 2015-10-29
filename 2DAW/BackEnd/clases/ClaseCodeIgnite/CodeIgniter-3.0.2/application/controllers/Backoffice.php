f26efefa70279988f27c21e82741f01a264cd207
Edgar S. Hurtado <edsanhu@gmail.com>
2015-10-29 21:47:27 +0100

    case 29/10/2015

--- 2DAW/BackEnd/clases/ClaseCodeIgnite/CodeIgniter-3.0.2/application/controllers/Backoffice.php
+++ 2DAW/BackEnd/clases/ClaseCodeIgnite/CodeIgniter-3.0.2/application/controllers/Backoffice.php
@@ -6,7 +6,8 @@ class Backoffice extends CI_Controller {
   function __construct() {
     parent::__construct();
     //Cargar el modelo
-    $this->load->helper('url');
+    $this->load->helper('url'); //Carga la función site_url().
+    $this->load->database();
   }
 
   public function prueba()
@@ -21,7 +22,18 @@ class Backoffice extends CI_Controller {
 
     $user = $this->input->post('username');
     $pass = $this->input->post('password');
-    echo "$user $pass";
+    // echo "$user $pass";
+
+    $sql = "SELECT * FROM cg_users
+            WHERE user_name='$user' AND user_password='$pass'";
+
+    $query = $this->db->query($sql);
+
+    if ($query->num_rows() == 1) {
+      $this->load->view("backoffice/admin/pages/index.html");
+    } else {
+      $this->index();
+    }
   }
 
   public function index()
