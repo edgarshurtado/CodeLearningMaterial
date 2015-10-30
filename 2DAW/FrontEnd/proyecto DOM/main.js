bb8adce9661e60d727995191d2cb4624e28f60e5
Edgar S. Hurtado <edsanhu@gmail.com>
2015-10-30 18:55:30 +0100

    Refactor for not having code in tags

--- 2DAW/FrontEnd/proyecto DOM/main.js
+++ 2DAW/FrontEnd/proyecto DOM/main.js
@@ -1,58 +1,81 @@
-function reveal(triggerId, target){
-  triggerElement = document.getElementById(triggerId);
-  targetElement = document.getElementById(target);
-  console.log(target);
-  return function(){
-    console.log(targetElement);
-    targetElement.className = 'visible';
-
-    switch(triggerId){
-      case "threshLink": changeImage("./image/Thresh_0.jpg");
-        break;
-      case "sonaLink": changeImage("./image/Sona_0.jpg");
-        break;
-    }
-  }
-}
-
 function changeImage(imageUrl){
   document.getElementById('imageHolder').src = imageUrl;
 }
 
-window.onload = function(){
+function alertMessage(id){
+  var parent = document.getElementById(id);
+  var newChild = document.createElement('p');
+  newChild.className = 'important';
+  var text = document.createTextNode('Remember that AP supports are squissy');
 
-  document.getElementById('threshLink').onmouseover = reveal('threshLink', 'threshExtra');
-
-    document.getElementById('sonaLink').onmouseover = reveal('sonaLink', 'sonaExtra');
+  newChild.appendChild(text);
+  parent.insertBefore(newChild, parent.getElementsByTagName('p')[1]);
 }
 
-// function setHide(spanElement){
-//   (spanElement).className = 'hidden';
-// }
+function destroyMessage(id){
+  var parent = document.getElementById(id);
+
+  parent.removeChild(parent.getElementsByTagName('p')[1]);
+}
+
+function counters(countersArray){
+  var message = "This champ counters are :\n"
+
+  for (var i = 0; i < countersArray.length; i++) {
+    message +="-- " + countersArray[i] + "\n";
+  };
+
+  return function(){
+    alert(message);
+  }
+}
+
+function reveal(divID, targetId){
+
+   var spanElement = document.getElementById(targetId);
+   var divElement = document.getElementById(divID);
+   function action(){
+    spanElement.className = 'visible';
+    if(divElement.className == "ExtraInfo"){
+      alertMessage(divID);
+    }
+    switch(divID){
+      case "threshDiv": changeImage("./image/Thresh_0.jpg");
+        break;
+      case "sonaDiv": changeImage("./image/Sona_0.jpg");
+        break;
+    }
+  }
+
+  return action;
+}
+
+function hide(divID, targetId){
+
+   var spanElement = document.getElementById(targetId);
+   var divElement = document.getElementById(divID);
+
+   function action(){
+    spanElement.className = 'hidden';
+    if(divElement.className == "ExtraInfo"){
+      destroyMessage(divID);
+    }
+  }
+
+  return action;
+}
 
 
+window.onload = function(){
 
-// function alertMessage(id){
-//   var parent = document.getElementById(id);
-//   var newChild = document.createElement('p');
-//   newChild.className = 'important';
-//   var text = document.createTextNode('Remember that AP supports are squissy');
+  document.getElementById('threshLink').onmouseover = reveal('threshDiv', "threshExtra");
 
-//   newChild.appendChild(text);
-//   parent.insertBefore(newChild, parent.getElementsByTagName('p')[1]);
-// }
+    document.getElementById('sonaLink').onmouseover = reveal('sonaDiv', 'sonaExtra');
 
-// function destroyMessage(id){
-//   var parent = document.getElementById(id);
+    document.getElementById('threshLink').onmouseout = hide('threshDiv', "threshExtra");
+    document.getElementById('sonaLink').onmouseout = hide('sonaDiv', 'sonaExtra');
 
-//   parent.removeChild(parent.getElementsByTagName('p')[1]);
-// }
 
-// function counters(countersArray){
-//   var message = "This champ counters are :\n"
-
-//   for (var i = 0; i < countersArray.length; i++) {
-//     message +="-- " + countersArray[i] + "\n";
-//   };
-//   alert(message);
-// }
+    document.getElementById('threshCounters').onclick = counters(['Janna', 'Soraka']);
+    document.getElementById('sonaCounters').onclick = counters(['Thresh', 'Alistar']);
+}
