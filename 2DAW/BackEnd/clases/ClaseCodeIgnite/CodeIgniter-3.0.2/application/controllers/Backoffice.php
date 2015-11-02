2484d8b4492882a86d27c5728f4bbd744d4b80ee
Edgar S. Hurtado <edsanhu@gmail.com>
2015-11-02 20:54:55 +0100

    Add class 2/11/2015

--- 2DAW/BackEnd/clases/ClaseCodeIgnite/CodeIgniter-3.0.2/application/controllers/Backoffice.php
+++ 2DAW/BackEnd/clases/ClaseCodeIgnite/CodeIgniter-3.0.2/application/controllers/Backoffice.php
@@ -8,6 +8,12 @@ class Backoffice extends CI_Controller {
     //Cargar el modelo
     $this->load->helper('url'); //Carga la función site_url().
     $this->load->database();
+
+    $this->load->library('session');
+    $this->load->library('encrypt');
+
+    //Cargar librería de CRUD
+    $this->load->library('grocery_CRUD');
   }
 
   public function prueba()
@@ -25,15 +31,80 @@ class Backoffice extends CI_Controller {
     // echo "$user $pass";
 
     $sql = "SELECT * FROM cg_users
-            WHERE user_name='$user' AND user_password='$pass'";
+            WHERE user_name=? AND user_password=?";
 
-    $query = $this->db->query($sql);
+    $query = $this->db->query($sql, array($user, $pass));
 
     if ($query->num_rows() == 1) {
-      $this->load->view("backoffice/admin/pages/index.html");
+      $row = $query->row();
+      $description = $row->user_description;
+      $email = $row->user_email;
+
+      //Guardamos las variables de sesión
+      $variables_sesion=array(
+          'user_name' => $user,
+          'user_description' => $description,
+          'user_email' => $email);
+
+      $this->session->set_userdata($variables_sesion);
+
+      //Cargamos la página de administración
+      $this->load->view("backoffice/admin/pages/index.php");
     } else {
       $this->index();
     }
+  }
+  public function logout()
+  {
+      // $variables_sesion=array(
+      //     'user_name' => "",
+      //     'user_description' => "",
+      //     'user_email' => "");
+
+      // $this->session->unset_userdata($variables_sesion);
+      // $this->index();
+      $this->session->sess_destroy();
+      $this->index();
+  }
+
+  public function usuarios()
+  {
+    $crud = new Grocery_CRUD();
+    $crud->set_table('cg_users');
+    $output = $crud->render();
+
+    // print_r($output);
+
+    $this->load->view("backoffice/example", $output);
+  }
+
+  public function participantes()
+  {
+    // Esto es más corto porque le hemos puesto a la función el mismo nombre
+    // que la tabla
+    $output = $this->grocery_crud->render();
+    $this->load->view("backoffice/example", $output);
+  }
+
+  public function tiendas()
+  {
+    $crud = new Grocery_CRUD();
+    $crud->set_table('cg_store');
+    $crud->columns( 'store_name', 'store_address', 'store_city', 'store_state','store_logo', 'store_area_id');
+
+    $crud->display_as('store_name', 'Nombre tienda');
+    $crud->display_as('store_adress', 'Dirección tienda');
+
+    $crud->set_field_upload('store_logo', 'assets/uploads/files');
+    $crud->set_relation('store_area_id', 'cg_area_store', 'area_store_name');
+
+    $output = $crud->render();
+    $this->load->view("backoffice/example", $output);
+  }
+
+  public function verVariables()
+  {
+    echo "description:" . $this->session->userdata('user_description');
   }
 
   public function index()
