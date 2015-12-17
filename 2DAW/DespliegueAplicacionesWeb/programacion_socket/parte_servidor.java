253fc4c5cdc67993ca4d8c714fb836d47ba05123
Edgar S. Hurtado <edsanhu@gmail.com>
2015-12-17 19:03:50 +0100

    Add Server socket file

---
+++ 2DAW/DespliegueAplicacionesWeb/programacion_socket/parte_servidor.java
@@ -0,0 +1,15 @@
+import java.io*;
+import java.net*;
+
+public class Parte_servidor {
+    static final int PORT = 7000;
+    public static void main(String[] args) {
+        String fraseCliente,
+               String FraseMayusculas;
+        ServerSocket SocketAcogida = new ServerSocket(PORT);
+
+        while(true){
+            // Programar aquí
+        }
+    }
+}
