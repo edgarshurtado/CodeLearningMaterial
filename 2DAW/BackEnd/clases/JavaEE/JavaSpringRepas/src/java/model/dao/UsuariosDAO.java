c3ba228a20f63e4c60c382b031567ecba89da8ae
Edgar S. Hurtado <edsanhu@gmail.com>
2016-02-12 20:54:03 +0100

    Implementa metodo getAllUsers

--- 2DAW/BackEnd/clases/JavaEE/JavaSpringRepas/src/java/model/dao/UsuariosDAO.java
+++ 2DAW/BackEnd/clases/JavaEE/JavaSpringRepas/src/java/model/dao/UsuariosDAO.java
@@ -5,10 +5,35 @@
  */
 package model.dao;
 
+import java.util.ArrayList;
+import model.entidad.Usuarios;
+import model.util.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 /**
  *
  * @author Rumil
  */
 public class UsuariosDAO {
+	/*
+	Obtiene todos los usuarios de la base de datos y los devuelve en un arraylist
+	*/
+	public static ArrayList<Usuarios> getAllUsers(){
+		
+		ArrayList<Usuarios> lista = null;
+		
+		try {
+			Session session = HibernateUtil.getSessionFactory().openSession();
+			String hql = "SELECT u FROM Usuarios u";
+
+			Query query = session.createQuery(hql);
+
+			lista = (ArrayList<Usuarios>)query.list();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return lista;
+	}
 	
 }
