2aad2ddbfbd63398aa3fa6ad1c0424f9846a55c3
Edgar S. Hurtado <edsanhu@gmail.com>
2016-02-12 21:00:18 +0100

    Crea UsuariosController

---
+++ 2DAW/BackEnd/clases/JavaEE/JavaSpringRepas/src/java/controllers/UsuariosController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ *
+ * @author Rumil
+ */
+public class UsuariosController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+}
