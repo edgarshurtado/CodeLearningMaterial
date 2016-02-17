683a360ac32c2192e545c1b5507265655a539ef1
Edgar S. Hurtado <edsanhu@gmail.com>
2016-02-17 10:56:16 +0100

    Add visualization of movies names. Work with promisses

--- MOOCs/AngularJS/ProyectoCursoAngular/scripts/controllers/peliculasProximamenteCtrl.js
+++ MOOCs/AngularJS/ProyectoCursoAngular/scripts/controllers/peliculasProximamenteCtrl.js
@@ -2,6 +2,14 @@
  * Created by Rumil on 16/02/16.
  */
 
-angular.module("MisPelisSeriesApp").controller("peliculasProximamenteCtrl", ["$scope", function($scope){
-    // Algo
-}])
+angular.module("MisPelisSeriesApp").controller("peliculasProximamenteCtrl", ["$scope", "$http", function($scope, $http){
+    $http
+        .get("https://api.themoviedb.org/3/movie/upcoming?api_key=e08466e6994d3713b0a8b64245164762&language=es")
+        .then(
+            function(resultado) {
+                $scope.peliculas = resultado.data.results;
+            },
+            function(){
+                alert("Algo no ha ido bien");
+            });
+}]);
