f79a31e2900b34dbef58355b73ec5fc8f09a4a8f
Edgar S. Hurtado <edsanhu@gmail.com>
2016-02-22 10:44:25 +0100

    Fix movie poster image
    TheMovieDataBase seems to have just some width values allowed.

--- MOOCs/AngularJS/ProyectoCursoAngular/scripts/controllers/peliculasDetalleCtrl.js
+++ MOOCs/AngularJS/ProyectoCursoAngular/scripts/controllers/peliculasDetalleCtrl.js
@@ -6,8 +6,9 @@ angular.module("MisPelisSeriesApp").controller("peliculasDetalleCtrl",
     ["$scope", "Pelicula", "MovieDbApi" ,function($scope, Pelicula, MovieDbApi){
 
         $scope.pelicula = Pelicula.data;
+        debugger;
 
         $scope.rutaImagen = function(ruta) {
-            return MovieDbApi.obtenerRutaImagen(115, ruta);
+            return MovieDbApi.obtenerRutaImagen(150, ruta);
         };
 }]);
