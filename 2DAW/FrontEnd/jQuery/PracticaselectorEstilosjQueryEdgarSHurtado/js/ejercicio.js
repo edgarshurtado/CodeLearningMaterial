017a391d8a9f38f158bb9e6d84e94046c2a726ac
Edgar S. Hurtado <edsanhu@gmail.com>
2016-02-24 09:12:41 +0100

    Finish selectors exercise

--- 2DAW/FrontEnd/jQuery/PracticaselectorEstilosjQueryEdgarSHurtado/js/ejercicio.js
+++ 2DAW/FrontEnd/jQuery/PracticaselectorEstilosjQueryEdgarSHurtado/js/ejercicio.js
@@ -1,3 +1,22 @@
 /**
  * Created by Edgar S. Hurtado on 23/02/16.
  */
+
+$('#btn_aplicar').click(function(){
+    var $classToApply = $('#estilo-aplicar').val();
+
+    var domSeleccionado = $('[name="elementosSeleccion"]:checked');
+
+    domSeleccionado.each(function(){
+        var elementos = $($(this).val());
+        elementos.each(function(){
+            $(this).addClass($classToApply);
+        })
+    })
+});
+
+$('#btn_nomalidad').click(function(){
+    $('.red').removeClass('red');
+    $('.blue').removeClass('blue');
+    $('.green').removeClass('green');
+})
