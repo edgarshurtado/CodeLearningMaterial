24f0aaa1e331c36ce9a64601ff2dabe4eea93c2b
Edgar S. Hurtado <edsanhu@gmail.com>
2016-03-28 13:52:06 +0200

    Add function 2 assigment 1

--- MOOCs/R Coursera/specdata/pollutantmean.R
+++ MOOCs/R Coursera/specdata/pollutantmean.R
@@ -9,3 +9,19 @@ pollutantmean <- function(directory = getwd(), pollutant, id = 1:322){
     }
     mean(result, na.rm = TRUE)
 }
+
+nComplete <- function(file) {
+    data <- read.csv(file)
+    compCases <- complete.cases(data)
+    length(compCases[compCases == TRUE])
+}
+
+complete <- function(directory = ".", id = 1:332){
+    files <- list.files(directory, full.names = TRUE)[id]
+    resultDataFrame <- data.frame("complete.cases" = numeric(0))
+    for(file in files){
+        tempDataFrame <- data.frame("complete.cases" = nComplete(file), row.names = file)
+        resultDataFrame <- rbind(resultDataFrame, tempDataFrame)
+    }
+    resultDataFrame
+}
